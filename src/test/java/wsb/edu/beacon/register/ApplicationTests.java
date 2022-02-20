f727ee6a0b6119b54b577efe818e261e8b802df3
sigamateusz <sigamateusz@gmail.com>
2022-02-20 20:49:58 +0100

    first commit

---
+++ src/test/java/wsb/edu/beacon/register/ApplicationTests.java
@@ -0,0 +1,13 @@
+package wsb.edu.beacon.register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
